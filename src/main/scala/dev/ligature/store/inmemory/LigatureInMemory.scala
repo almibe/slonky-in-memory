474d9ce7ca1e64467a7d7de72e5df76bfd6fb838
berry3 <berry3@indiana.edu>
2020-09-08 23:39:41 -0400

    getting first test to pass, #1

--- src/main/scala/dev/ligature/store/inmemory/LigatureInMemory.scala
+++ src/main/scala/dev/ligature/store/inmemory/LigatureInMemory.scala
@@ -58,22 +58,33 @@ private final class InMemoryLigatureSession extends LigatureSession {
 
 private final class InMemoryReadTx(private val data: Atomic[Map[String, Collection]]) extends ReadTx {
   def allStatements(collection: LocalNode): Observable[PersistedStatement] = ???
-  def collections(): Observable[LocalNode] = ???
+
+  def collections(): Observable[LocalNode] = Observable.fromIterable(data.get.map { v => LocalNode(v._1) })
+
   def collections(prefix: LocalNode): Observable[LocalNode] = ???
+
   def collections(from: LocalNode, to: LocalNode): Observable[LocalNode] = ???
+
   def matchStatements(collection: LocalNode,
                       subject: Option[Node],
                       predicate: Option[NamedNode],
                       `object`: Option[Object]): Observable[PersistedStatement] = ???
+
   def statementByContext(collection: LocalNode, context: AnonymousNode): Task[Option[PersistedStatement]] = ???
 }
 
 private final class InMemoryWriteTx(private val data: Atomic[Map[String, Collection]]) extends WriteTx {
   private val isOpen = Atomic(true)
+
   def addStatement(collection: LocalNode, statement: Statement): Task[PersistedStatement] = ???
+
   def cancel(): Unit = ???
+
   def close(): Unit = ???
+
   def createCollection(collection: LocalNode): Task[LocalNode] = ???
+
   def deleteCollection(collection: LocalNode): Task[LocalNode] = ???
+
   def newEntity(collection: LocalNode): Task[AnonymousNode] = ???
 }
