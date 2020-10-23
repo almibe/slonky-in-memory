6e8496cbce2f582cb15a123ab12079eff9cf7e25
berry3 <berry3@indiana.edu>
2020-10-22 23:42:09 -0400

    updating to use new slonky api + getting first test to fail, #12

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 use slonky::Slonky;
 use slonky::ReadTx;
 use slonky::WriteTx;
-use futures::stream::Stream;
-use async_trait::async_trait;
 
 struct InMemorySlonky {
 
@@ -20,74 +18,71 @@ struct InMemoryWriteTx {
 
 }
 
-#[async_trait]
 impl Slonky for InMemorySlonky {
-    async fn read<T, E>(&self, f: Box<dyn Fn(dyn ReadTx) -> Result<T, E>  + Sync + Send>) -> Result<T, E> {
+    fn read<T, E>(&self, f: Box<dyn Fn(Box<dyn ReadTx>) -> Result<T, E>  + Sync + Send>) -> Result<T, E> {
         unimplemented!()
     }
 
-    async fn write<E>(&self, f: Box<dyn Fn(dyn WriteTx) -> Result<(), E> + Sync + Send>) -> Result<(), E> {
+    fn write<E>(&self, f: Box<dyn Fn(Box<dyn WriteTx>) -> Result<(), E> + Sync + Send>) -> Result<(), E> {
         unimplemented!()
     }
 }
 
-#[async_trait]
 impl ReadTx for InMemoryReadTx {
-    async fn key_exists(&self, key: &[u8]) -> bool {
+    fn key_exists(&self, key: &[u8]) -> bool {
         unimplemented!()
     }
 
-    async fn prefix_exists(&self, prefix: &[u8]) -> bool {
+    fn prefix_exists(&self, prefix: &[u8]) -> bool {
         unimplemented!()
     }
 
-    async fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
         unimplemented!()
     }
 
-    async fn prefix_scan(&self, prefix: &[u8]) -> Box<dyn Stream<Item=(Vec<u8>, Vec<u8>)>> {
+    fn prefix_scan(&self, prefix: &[u8]) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>> {
         unimplemented!()
     }
 
-    async fn range_scan(&self, from: &[u8], to: &[u8]) -> Box<dyn Stream<Item=(Vec<u8>, Vec<u8>)>> {
+    fn range_scan(&self, from: &[u8], to: &[u8]) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>> {
         unimplemented!()
     }
 
-    async fn scan_all(&self) -> Box<dyn Stream<Item=(Vec<u8>, Vec<u8>)>> {
+    fn scan_all(&self) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>> {
         unimplemented!()
     }
 }
 
-#[async_trait]
 impl WriteTx for InMemoryWriteTx {
-    async fn key_exists(&self, key: &[u8]) -> bool {
+    fn key_exists(&self, key: &[u8]) -> bool {
         unimplemented!()
     }
 
-    async fn prefix_exists(&self, prefix: &[u8]) -> bool {
+    fn prefix_exists(&self, prefix: &[u8]) -> bool {
         unimplemented!()
     }
 
-    async fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
         unimplemented!()
     }
 
-    async fn put(&self, key: &[u8], value: &[u8]) -> (Vec<u8>, Vec<u8>) {
+    fn put(&self, key: &[u8], value: &[u8]) -> (Vec<u8>, Vec<u8>) {
         unimplemented!()
     }
 
-    async fn remove(&self, key: &[u8]) -> (Vec<u8>, Vec<u8>) {
+    fn remove(&self, key: &[u8]) -> (Vec<u8>, Vec<u8>) {
         unimplemented!()
     }
 
-    async fn cancel(&self) {
+    fn cancel(&self) {
         unimplemented!()
     }
 }
 
-// pub fn create_in_memory_slonky() -> impl Slonky {
-//     unimplemented!()
-// }
+pub fn create_in_memory_slonky() -> impl Slonky {
+    InMemorySlonky {}
+}
 
 #[cfg(test)]
 mod test;
