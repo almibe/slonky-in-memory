e6150446d11c3a75bdece0dead8d2c94046397dc
berry3 <berry3@indiana.edu>
2021-01-11 18:41:22 -0500

    removing the last of scala code, working on tests still, closes #17, #18

--- tests/tests.rs
+++ tests/tests.rs
@@ -50,44 +50,52 @@ mod tests {
    fn check_if_a_key_exists_during_and_after_a_write() {
     let slonky = SlonkyInMemory::new();
 
-     let  (writeExists, writeExists2, readExists) = createSlonky.instance.use { instance =>
-       for {
-         writeExists <- instance.write.use { tx =>
-           for {
-             _           <- tx.put(hex"ABAB", hex"CD")
-             writeExists <- tx.keyExists(hex"ABAB")
-           } yield writeExists
-         }
-         writeExists2 <- instance.write.use { tx =>
-           tx.keyExists(hex"ABAB")
-         }
-         readExists <- instance.read.use { tx =>
-           tx.keyExists(hex"ABAB")
-         }
-       } yield (writeExists, writeExists2, readExists)
-     }.unsafeRunSync()
-     assert!(writeExists)
-     assert!(writeExists2)
-     assert!(readExists)
+    let write_tx1 = slonky.write();
+
+    tx.put(hex"ABAB", hex"CD");
+    writeExists <- tx.keyExists(hex"ABAB");
+
+
+    write_tx1.commit();
+    let write_tx2 = slonky.write();
+    tx.keyExists(hex"ABAB");
+
+    write_tx2.commit();
+    let read_tx = slonky.read();
+    let read_exists1 = tx.keyExists(hex"ABAB");
+    let read_exists2 =tx.key_exists(hex"AAAA");
+
+     assert!(writeExists);
+     assert!(writeExists2);
+     assert!(readExists1);
+     assert!(!read_exists2);
    }
  
    #[test]
    fn remove_a_pair() {
     let slonky = SlonkyInMemory::new();
 
-     let  res = createSlonky.instance.use { instance =>
-       for {
-         _ <- instance.write.use { tx =>
-           tx.put(hex"ABAB", hex"CD")
-         }
-         _ <- instance.write.use { tx =>
-           tx.remove(hex"ABAB")
-         }
-         all <- instance.read.use { tx =>
-           tx.scanAll().compile.toList
-         }
-       } yield all
-     }.unsafeRunSync().toSet
+    let write_tx1 = slonky.write();
+    tx.put(hex"ABAB", hex"CD");
+    tx.put(hex"AAAB", hex"DD");
+    tx.remove(hex"AAAB");
+
+
+    write_tx1.commit();
+
+    let write_tx2 = slonky.write();
+    //TODO check read
+    tx.remove(hex"ABAB");
+    //TODO check read
+    tx.remove("ACCC");
+    write_tx2.commit();
+
+
+
+    let read_tx = slonky.read();
+    tx.scanAll().compile.toList
+
+    //TODO probably have more asserts
      assert!(res.isEmpty)
    }
  
@@ -95,97 +103,77 @@ mod tests {
    fn prefix_exists() {
     let slonky = SlonkyInMemory::new();
 
-     let  (writeExists, writeExists2, readExists) = createSlonky.instance.use { instance =>
-       for {
-         writeExists <- instance.write.use { tx =>
-           for {
-             _ <- tx.put(hex"ABAB", hex"CD")
-             _ <- tx.put(hex"ABAC", hex"DC")
-             _ <- tx.put(hex"CAAB", hex"DE")
-             writeExists <- tx.prefixExists(hex"ABAC")
-           } yield writeExists
-         }
-         writeExists2 <- instance.write.use { tx =>
-           tx.prefixExists(hex"ABAB")
-         }
-         readExists <- instance.read.use { tx =>
-           tx.prefixExists(hex"CA")
-         }
-       } yield (writeExists, writeExists2, readExists)
-     }.unsafeRunSync()
-     assert!(writeExists)
-     assert!(writeExists2)
-     assert!(readExists)
+    let write_tx1 = slonky.write();
+    tx.put(hex"ABAB", hex"CD");
+    tx.put(hex"ABAC", hex"DC");
+    tx.put(hex"CAAB", hex"DE");
+    let write_exists1 = tx.prefixExists(hex"ABAC");
+
+
+    write_tx1.commit();
+    let write_tx2 = slonky.write();
+    let write_exists2 = tx.prefixExists(hex"ABAB");
+    write_tx2.commit();
+
+    let read_tx = slonky.read();
+    tx.prefixExists(hex"CA");
+
+     assert!(writeExists);
+     assert!(writeExists2);
+     assert!(readExists);
    }
  
    #[test]
    fn pre_scan() {
     let slonky = SlonkyInMemory::new();
 
-     let  prefixScan = createSlonky.instance.use { instance =>
-       for {
-         _ <- instance.write.use { tx =>
-           for {
-             _ <- tx.put(hex"ABAB", hex"CD")
-             _ <- tx.put(hex"ABAC", hex"DC")
-             _ <- tx.put(hex"CAAB", hex"DE")
-           } yield ()
-         }
-         prefixScan <- instance.read.use { tx =>
-           tx.prefixScan(hex"AB").compile.toList
-         }
-       } yield (prefixScan)
-     }.unsafeRunSync()
-     assert_eq!(prefixScan.toSet, Set((hex"ABAB", hex"CD"), (hex"ABAC", hex"DC")))
+    let write_tx = slonky.write();
+    write_tx.put(hex"ABAB", hex"CD");
+    write_tx.put(hex"ABAC", hex"DC");
+    write_tx.put(hex"CAAB", hex"DE");
+    write_tx.commit();
+
+    let read_tx = slonky.read();
+    let prefix_scan = tx.prefixScan(hex"AB").compile.toList;
+
+    assert_eq!(prefixScan.toSet, Set((hex"ABAB", hex"CD"), (hex"ABAC", hex"DC")));
    }
  
    #[test]
    fn ranges() {
     let slonky = SlonkyInMemory::new();
 
-     let  res = createSlonky.instance.use { instance =>
-       for {
-         _ <- instance.write.use { tx =>
-           for {
-             _ <- tx.put(hex"AAAA", hex"AABB")
-             _ <- tx.put(hex"ABAB", hex"BC")
-             _ <- tx.put(hex"ABBB", hex"CD")
-             _ <- tx.put(hex"ADDD", hex"DDDD")
-             _ <- tx.put(hex"CAAB", hex"DEAD")
-           } yield ()
-         }
-         rangeScan <- instance.read.use { tx =>
-           tx.rangeScan(hex"AB", hex"C0").compile.toList
-         }
-       } yield rangeScan
-     }.unsafeRunSync()
-     assert_eq!(res.toSet, Set((hex"ABAB", hex"BC"), (hex"ABBB", hex"CD"), (hex"ADDD", hex"DDDD")))
+    let write_tx = slonky.write();
+    write_tx.put(hex"AAAA", hex"AABB");
+    write_tx.put(hex"ABAB", hex"BC");
+    write_tx.put(hex"ABBB", hex"CD");
+    write_tx.put(hex"ADDD", hex"DDDD");
+    write_tx.put(hex"CAAB", hex"DEAD");
+    write_tx.commit();
+
+    let read_tx = slonky.read();
+    let res = tx.rangeScan(hex"AB", hex"C0").collect();
+
+     assert_eq!(res.toSet, Set((hex"ABAB", hex"BC"), (hex"ABBB", hex"CD"), (hex"ADDD", hex"DDDD")));
    }
  
    #[test]
    fn canceling_writes() {
     let slonky = SlonkyInMemory::new();
 
-     let  res = createSlonky.instance.use { instance =>
-       for {
-         _ <- instance.write.use { tx =>
-           for {
-             _ <- tx.put(hex"AB", hex"CD")
-             _ <- tx.put(hex"AC", hex"DC")
-           } yield ()
-         }
-         _ <- instance.write.use { tx =>
-           for {
-             _ <- tx.put(hex"ADDD", hex"DDDD")
-             _ <- tx.put(hex"CAAB", hex"DEAD")
-             _ <- tx.cancel()
-           } yield ()
-         }
-         all <- instance.read.use { tx =>
-           tx.scanAll().compile.toList
-         }
-       } yield all
-     }.unsafeRunSync()
-     assert_eq!(res.toSet, Set((hex"AB", hex"CD"), (hex"AC", hex"DC")))
+    let write_tx1 = slonky.write();
+    _ <- tx.put(hex"AB", hex"CD");
+    _ <- tx.put(hex"AC", hex"DC");
+    write.commit();
+
+    let write_tx2 = slonky.write();
+    tx.put(hex"ADDD", hex"DDDD");
+    tx.put(hex"CAAB", hex"DEAD");
+    write_tx2.cancel();
+
+    let read_tx = slonky.read();
+    let res = tx.scanAll().collect();
+
+     assert_eq!(res.toSet, Set((hex"AB", hex"CD"), (hex"AC", hex"DC")));
    }
 }
